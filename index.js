9c86cf57d2550412d6e5ea6f77f13db51d14fe2c
Hirokosama <hiroko.horiuchi@outlook.com>
2024-10-26 10:43:35 -0600

    Upload my QA portfolio

---
+++ index.js
@@ -0,0 +1,10 @@
+const greeting = document.getElementById("greeting")
+
+greeting.style.textAlign = "center"
+greeting.style.marginTop = "50px"
+
+const username = window.prompt("What's your name?", "Hiroko");  
+greeting.innerHTML = "Welcome to my website, " + username + "!"
+greeting.style.fontFamily = "Arial"
+greeting.style.fontSize = "40px"
+greeting.style.color = "#87ceeb"
